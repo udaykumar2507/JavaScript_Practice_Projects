1dea6a269dc3771f21ad08945429965eddd5ec80
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2023-12-08 23:20:03 +0530

    Delete Hypotenus directory

--- Hypotenus/index.js
+++
@@ -1,9 +0,0 @@
-let a;
-let b;
-let c;
-document.getElementById("mybutton").onclick=function(){
-    a=document.getElementById("input1").value;
-    b=document.getElementById("input2").value;
-    c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
-    document.getElementById("c").innerHTML="The Value of C is "+c;
-}
